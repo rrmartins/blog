ce3874e582935a076be91f6f0bbf5a6de4841248
Rodrigo Martins <rodrigo@rrmartins.com>
2011-10-16 23:13:08 -0200

    More tests! Check how to use RSpec to Devise!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_filter :authenticate_user!, :except => [:index, :show]
+  before_filter :authenticate_user!, :except => [:index, :show, :create]
   # GET /posts
   # GET /posts.xml
   def index
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -9,11 +9,24 @@ describe PostsController do
     end
   end
 
-  describe "GET 'show'" do
-    it "should be successful" do
-      get 'show'
+    it "POST create, deveria criar post" do
+      @post = Post.new
+      @post.titulo = "Titulo"
+      @post.chamada = "Chamada"
+      @post.texto = "Texto"
+      post :create, :post => @post
       response.should be_success
     end
+
+    it " show should be successful" do
+      get :show, :id => 1
+      response.should be_success
+    end
+    
+    
+  
+  it "uma instancia" do
+    controller.should be_an_instance_of(PostsController)
   end  
 
 end
