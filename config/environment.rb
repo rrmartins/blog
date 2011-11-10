d59cbceb5b831b12fd9ae73bb376599e9ed55606
Rodrigo Martins <rodrigo@rrmartins.com>
2011-11-10 07:47:33 -0200

    alterado local de envio de e-mail

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,12 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Blog::Application.initialize!
 # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+ActionMailer::Base.smtp_settings = {
+                    :address        => "smtp.gmail.com",
+                    :port           => 587,
+                    :authentication => :plain,
+                    :user_name      => "rodrigo.rr.mart@gmail.com",
+                    :password       => "09042008N@"
+    }
+
